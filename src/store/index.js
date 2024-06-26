8241c822ee2e490c06091faf8a9fd2e8c17085af
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-06-26 15:46:50 +0200

    store setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,153 @@
-import { createStore } from 'vuex'
+import {
+  createStore
+} from 'vuex'
+import axios from 'axios'
+import Swal from 'sweetalert2/dist/sweetalert2'
+
+const dataURL = 'https://ismaaeel-a.github.io/Vue_EOMP/data'
 
 export default createStore({
   state: {
+    jobTitle: null,
+    about: null,
+    education: null,
+    skills: null,
+    projects: null,
+    testimonials: null,
+
   },
   getters: {
+
   },
   mutations: {
+    setJobtitle(state, value) {
+      state.jobTitle = value
+    },
+    setAbout(state, value) {
+      state.about = value
+    },
+    setEducation(state, value) {
+      state.education = value
+    },
+    setSkills(state, value) {
+      state.skills = value
+    },
+    setProjects(state, value) {
+      state.projects = value
+    },
+    setTestimonials(state, value) {
+      state.testimonials = value
+    }
+
   },
   actions: {
+    async fetchJobTitle(context) {
+      try {
+        let {
+          jobTitle
+        } = await (await axios.get(dataURL)).data
+        context.commit("setJobTitle", jobTitle)
+      } catch (e) {
+        Swal.fire({
+          title: "Error",
+          text: "Failed to fetch the job title",
+          icon: "error",
+          timer: 2000
+        })
+      }
+
+    },
+
+    async fetchAbout(context) {
+      try {
+        let {
+          about
+        } = await (await axios.get(dataURL)).data
+        context.commit("setAbout", about)
+      } catch (e) {
+        Swal.fire({
+          title: "Error",
+          text: "Failed to fetch the about",
+          icon: "error",
+          timer: 2000
+        })
+      }
+    },
+
+    async fetchEducation(context) {
+      try {
+        let {
+          education
+        } = await (await axios.get(dataURL)).data
+        context.commit("setEducation", education)
+      } catch (e) {
+        Swal.fire({
+          title: "Error",
+          text: "Failed to fetch the education",
+          icon: "error",
+          timer: 2000
+        })
+      }
+    },
+
+    async fetchSkills(context) {
+      try {
+        let {
+          skills
+        } = await (await axios.get(dataURL)).data
+        context.commit("setSkills", skills)
+      } catch (e) {
+        Swal.fire({
+          title: "Error",
+          text: "Failed to fetch the skills",
+          icon: "error",
+          timer: 2000
+        })
+      }
+    },
+
+    async fetchProjects(context) {
+      try {
+        let {
+          projects
+        } = await (await axios.get(dataURL)).data
+        context.commit("setProjects", projects)
+      } catch (e) {
+        Swal.fire({
+          title: "Error",
+          text: "Failed to fetch the projects",
+          icon: "error",
+          timer: 2000
+        })
+      }
+    },
+
+    async fetchTestimonials(context) {
+      try {
+        let {
+          testimonials
+        } = await (await axios.get(dataURL)).data
+        context.commit("setTestimonials", testimonials)
+      } catch (e) {
+        Swal.fire({
+          title: "Error",
+          text: "Failed to fetch the testimonials",
+          icon: "error",
+          timer: 2000
+        })
+      }
+
+
+
+
+
+
+
+    }
   },
   modules: {
+
   }
+
+
 })
