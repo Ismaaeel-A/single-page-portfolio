07cbccd88232cbd232aa60a24ae73c1654ea8974
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-06-28 14:59:20 +0200

    education & experience

---
+++ src/components/EducationSection.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div v-if="education?.length" class="row">
+        <h3>{{ education[0]?.heading }}</h3>
+
+        <div class="container">
+            <h2>{{education[0]?.subheading1}}</h2>
+            <p>{{ education[0]?.p1 }}</p>
+        </div>
+        <div class="container">
+            <h2>{{education[1]?.subheading2}}</h2>
+            <p>{{ education[1]?.p2 }}</p>
+        </div>
+        <div class="container">
+            <h2>{{education[2]?.subheading3}}</h2>
+            <p>{{ education[2]?.p3 }}</p>
+        </div>
+        <div class="container">
+            <h2>{{education[3]?.subheading4}}</h2>
+            <p>{{ education[3]?.p4 }}</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+/* import Spinner from './Spinner.vue' */
+
+const store = useStore()
+const education = computed(() => store.state.education)
+
+onMounted(() => {
+  store.dispatch('fetchEducation')
+  console.log('About Dispatch');
+})
+
+
+
+</script>
+
+
+
+
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
   </section>
 
   <section class="row" id="resume">
-
+    <EducationSection/>
   </section>
 
   <section class="row" id="project">
@@ -30,13 +30,14 @@
 // @ is an alias to /src
 import LandingSection from '../components/LandingSection.vue'
 import AboutSection from '../components/AboutSection.vue'
-
+import EducationSection from '../components/EducationSection.vue'
 
 export default {
   name: 'HomeView',
   components: {
     LandingSection,
-    AboutSection
+    AboutSection,
+    EducationSection
   }
 }
 </script>
